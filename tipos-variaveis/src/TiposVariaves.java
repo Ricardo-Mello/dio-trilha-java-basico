86fc9036bae27abdb05ac9f482210d8845e0394e
Ricardo-Mello <ricardomsmello@gmail.com>
2023-01-09 16:56:05 -0300

    Pt final e sempre em andamento do estudo de variáv

--- tipos-variaveis/src/TiposVariaves.java
+++ tipos-variaveis/src/TiposVariaves.java
@@ -1,5 +1,25 @@
 public class TiposVariaves {
     public static void main(String[] args) throws Exception {
+        // tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+
+        String meuNome = "RICARDO MELLO";
+
         System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.print(VALOR_DE_PI);
+
     }
 }
